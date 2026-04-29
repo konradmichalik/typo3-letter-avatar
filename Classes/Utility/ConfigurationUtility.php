82202611b252f4dd31cd3030b87c5337a37c705c
Konrad Michalik <km@move-elevator.de>
2026-04-29 09:13:35 +0200

    fix: update copyright year range to 2025-2026

--- Classes/Utility/ConfigurationUtility.php
+++ Classes/Utility/ConfigurationUtility.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 /*
  * This file is part of the TYPO3 CMS extension "typo3_letter_avatar".
  *
- * Copyright (C) 2025 Konrad Michalik <hej@konradmichalik.dev>
+ * Copyright (C) 2025-2026 Konrad Michalik <hej@konradmichalik.dev>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
